c9ba7afc7f8f773e480bf33c92f7d29c412ed9d8
Enoque Felipe <p000eleal@prservicos.com.br>
2017-11-21 14:32:34 -0200

    Test class comment

--- src/main/java/com/conceito/util/MainUtilsTest.java
+++ src/main/java/com/conceito/util/MainUtilsTest.java
@@ -1,33 +1,25 @@
 package com.conceito.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import junit.framework.TestCase;
-
-public class MainUtilsTest extends TestCase {
-
-	public List<String> agencias;
-	
-	@Before
-	public void criadorDeAgencias(){
-		this.agencias = new ArrayList<String>(); 
-		this.agencias.add("10");
-		this.agencias.add("11260");
-		this.agencias.add("170");
-	}
-	
-	@Test 
-	public void testeDeRemocaoDeZeroADireita(){
-		String valor = "11260";
-		assertEquals("1126", MainUtils.removeUltimoZero(valor));
-	}
-
-	@Test
-	public void testGeradorDeCombinacoes() {
-	}
+public class MainUtilsTest {
+//	public class MainUtilsTest extends TestCase {
+//	public List<String> agencias;
+//	
+//	@Before
+//	public void criadorDeAgencias(){
+//		this.agencias = new ArrayList<String>(); 
+//		this.agencias.add("10");
+//		this.agencias.add("11260");
+//		this.agencias.add("170");
+//	}
+//	
+//	@Test 
+//	public void testeDeRemocaoDeZeroADireita(){
+//		String valor = "11260";
+//		assertEquals("1126", MainUtils.removeUltimoZero(valor));
+//	}
+//
+//	@Test
+//	public void testGeradorDeCombinacoes() {
+//	}
 
 }
